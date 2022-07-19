43a655af895452fea6183bd3483794969e8dc21e
jeetmandal17 <jeet.mandal@swiggy.in>
2022-07-19 09:23:14 +0530

    Added code setup for graceful shutdown

--- EP1/working/main.go
+++ EP1/working/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"gRPC/EP1/handlers"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 
@@ -22,7 +25,30 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/g", gh)
 	sm.Handle("/c", ch)
-	 
-	http.ListenAndServe(":9090", sm)  
+	
+	s := http.Server{
+		Addr: ":9090",
+		Handler: sm,
+		IdleTimeout: 20*time.Second,
+		ReadTimeout: 1*time.Second,
+		WriteTimeout: 1*time.Second,
+	}
 
+
+	go func(){
+		err := s.ListenAndServe()  
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <- sigChan
+	l.Println("Received terminate, going graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
