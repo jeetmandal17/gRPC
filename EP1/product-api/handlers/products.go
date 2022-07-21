aabc4fc8ee4afc2964173106abd0b900e0a07f55
jeetmandal17 <jeet.mandal@swiggy.in>
2022-07-21 23:34:56 +0530

    added http get request in standaed format

--- EP1/product-api/handlers/products.go
+++ EP1/product-api/handlers/products.go
@@ -16,7 +16,18 @@ func NewProduct(l *log.Logger) *Products{
 	return &Products{l}
 }
 
+
+//This is the standard procedure to direct REST requests
 func  (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request){
+	if r.Method == http.MethodGet{
+		p.getProducts(rw,r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request){
 	lp := data.GetProducts()	
 	info, err := json.Marshal(lp)
 	if err != nil{
